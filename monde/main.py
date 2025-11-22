d0ffa6c95b12b34647132a05fabde445efe6e93d
gustofied <gustofied@gmail.com>
2025-11-22 23:39:23 +0100

    shaders inbounded

--- monde/main.py
+++ monde/main.py
@@ -2,6 +2,7 @@ import glfw
 import numpy as np
 import glfw.GLFW as GLFW_CONSTANTS
 from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
 import time
 
 
@@ -9,7 +10,31 @@ import time
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
+
+
+def create_shader_program(vertex_filepath: str, fragment_filepath: str) -> int:
+
+    vertex_module = create_shader_module(vertex_filepath, GL_VERTEX_SHADER)
+    fragment_module = create_shader_module(fragment_filepath, GL_FRAGMENT_SHADER)
+
+    shader = compileProgram(vertex_module, fragment_module)
+
+    glDeleteShader(vertex_module)
+    glDeleteShader(fragment_module)
+
+    return shader
+
+def create_shader_module(filepath: str, module_type) -> int:
+
+    source_code = ""
+    with open(filepath, "r") as file:
+        source_code = file.readlines()
+
+    return compileShader(source_code, module_type)
+    
 class App:
+
+
 
     def _set_color(self, r,g,b,a):
         glClearColor(r, g, b, a)
@@ -20,15 +45,27 @@ class App:
 
 
     def __init__(self):
-        glfw.init()
-        glfw.window_hint(
-        GLFW_CONSTANTS.GLFW_OPENGL_PROFILE, 
-       GLFW_CONSTANTS.GLFW_OPENGL_CORE_PROFILE)
-        self._set_opengl_version(3,3)
-        glfw.window_hint(GLFW_CONSTANTS.GLFW_OPENGL_FORWARD_COMPAT, GLFW_CONSTANTS.GLFW_TRUE)
-        self.window = glfw.create_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Monde", None, None)
-        glfw.make_context_current(self.window) 
+
+        self.initialize_glfw()
+        self.initialize_opengl()
+        
+    
+    
+    def initialize_glfw(self) -> None:    
+            glfw.init()
+            glfw.window_hint(
+            GLFW_CONSTANTS.GLFW_OPENGL_PROFILE, 
+        GLFW_CONSTANTS.GLFW_OPENGL_CORE_PROFILE)
+            self._set_opengl_version(3,3)
+            glfw.window_hint(GLFW_CONSTANTS.GLFW_OPENGL_FORWARD_COMPAT, GLFW_CONSTANTS.GLFW_TRUE)
+            self.window = glfw.create_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Monde", None, None)
+            glfw.make_context_current(self.window) 
+    
+    
+    def initialize_opengl(self) -> None:
         self._set_color(0.1,0.2,0.7,0.5)
+        self.VAO = glGenVertexArrays(1)
+        self.shader = create_shader_program("shaders/vertex.txt", "shaders/fragment.txt")
         
 
 
@@ -39,15 +76,21 @@ class App:
                 glfw.set_window_should_close(self.window, True) 
             glfw.poll_events()
             glClear(GL_COLOR_BUFFER_BIT)
-            glfw.swap_buffers(self.window)
             self._set_color(1,0,1,0.5)
             fps = 1 / (glfw.get_time() - time_end)
             time_end = glfw.get_time()
             glfw.set_window_title(self.window, F" fps = {str(fps)}") 
+            glUseProgram(self.shader)
+            glBindVertexArray(self.VAO)
+            glDrawArrays(GL_TRIANGLES, 0, 3)
+            glfw.swap_buffers(self.window)
+          
         self.quit()
             
                 
     def quit(self):
+        glDeleteVertexArrays(1, (self.VAO,))
+        glDeleteProgram(self.shader)
         glfw.destroy_window(self.window)
         glfw.terminate()
 
@@ -61,4 +104,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-        
