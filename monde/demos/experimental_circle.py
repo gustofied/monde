6c9f55535f266c953e8bad4bc1c07ed2aa1cde44
gustofied <gustofied@gmail.com>
2025-12-21 19:32:11 +0100

    donzo

--- monde/demos/experimental_circle.py
+++ monde/demos/experimental_circle.py
@@ -23,24 +23,42 @@ glfw.make_context_current(window)
 glfw.swap_interval(0)
 
 
-vertices = np.array(
-    [
-        [0.5, 0.5, 0.0],
-        [0.5, -0.5, 0.0],
-        [-0.5, -0.5, 0.0],
-    ],
-    dtype = np.float32
-) 
+# hollow circle 
 
-r = 1
-N = 64
+r_0 = 0.25
+N_0 = 12
 
-angles = np.linspace(0, 2*np.pi, N, endpoint=False).astype(np.float32)
-x_coordinates = r * np.cos(angles)
-y_coordinates = r * np.sin(angles)
-z_coordaintes = np.zeros_like(x_coordinates)
+angles_0 = np.linspace(0, 2*np.pi, N_0, endpoint=False).astype(np.float32)
+x_0_coordinates = r_0 * np.cos(angles_0) + 0.25
+y_0_coordinates = r_0 * np.sin(angles_0) + 0.25
+z_0_coordinates = np.zeros_like(x_0_coordinates)
 
-vertices = np.column_stack((x_coordinates, y_coordinates, z_coordaintes)).astype(np.float32)
+vertices_0 = np.stack((x_0_coordinates, y_0_coordinates, z_0_coordinates), axis= 1).astype(np.float32)
+
+# triangle, repeat circle
+
+r_1 = 0.5
+N_1 = 100
+
+angles_1 = np.linspace(0, 2*np.pi, N_1, endpoint=False).astype(np.float32)
+
+x_1_coordinates = r_1 * np.cos(angles_1) - 0.50
+y_1_coordinates = r_1 * np.sin(angles_1) - 0.50
+z_1_coordinates = np.zeros_like(x_1_coordinates)
+origo_1 = np.array([-0.5, -0.5, 0]).astype(np.float32)
+
+tri_vertices_1 = np.stack([x_1_coordinates, y_1_coordinates, z_1_coordinates], axis=1)
+
+triangles_1 = np.stack(
+    (tri_vertices_1, np.roll(tri_vertices_1, -1, axis=0), np.broadcast_to(origo_1, tri_vertices_1.shape)),
+    axis=1
+).astype(np.float32) 
+
+vertices_1 = triangles_1.reshape(-1, 3)
+
+print(vertices_1)
+
+# this is the ammount of attributes OpengGL can give on this mac?
 
 nr_attributes = glGetIntegerv(GL_MAX_VERTEX_ATTRIBS)
 print(nr_attributes)
@@ -87,16 +105,30 @@ glDeleteShader(fragmentShader)
 
 # VAO / VBO
 
-vao = glGenVertexArrays(1)
-vbo = glGenBuffers(1)
+# hollow circle
 
-glBindVertexArray(vao)
+vao_0 = glGenVertexArrays(1)
+vbo_0 = glGenBuffers(1)
 
-glBindBuffer(GL_ARRAY_BUFFER, vbo)
-glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+glBindVertexArray(vao_0)
+
+glBindBuffer(GL_ARRAY_BUFFER, vbo_0)
+glBufferData(GL_ARRAY_BUFFER, vertices_0.nbytes, vertices_0, GL_STATIC_DRAW)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 12, ctypes.c_void_p(0))
 glEnableVertexAttribArray(0)
 
+# triagle circle
+
+
+vao_1 = glGenVertexArrays(1)
+vbo_1 = glGenBuffers(1)
+
+glBindVertexArray(vao_1)
+
+glBindBuffer(GL_ARRAY_BUFFER, vbo_1)
+glBufferData(GL_ARRAY_BUFFER, vertices_1.nbytes, vertices_1, GL_STATIC_DRAW)
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 12, ctypes.c_void_p(0))
+glEnableVertexAttribArray(0)
 
 # render loop
 
@@ -111,10 +143,10 @@ while not glfw.window_should_close(window):
     glfw.set_window_title(window, f"Experimental")
 
     glUseProgram(shaderProgram)
-    glBindVertexArray(vao)
-    glDrawArrays(GL_LINE_LOOP, 0, 64)
-    glDrawArrays(GL_TRIANGLES, 0, 3)
-
+    glBindVertexArray(vao_0)
+    glDrawArrays(GL_LINE_STRIP, 0, N_0) # hollow circle # GL_LINE_LOOP instead too
+    glBindVertexArray(vao_1)
+    glDrawArrays(GL_TRIANGLES, 0, vertices_1.shape[0]- 10) # triangle  3*N_1
     
     # do stuff here
 
