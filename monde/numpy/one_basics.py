24d2bf49e31018a38a82267d448e1c2669453e76
gustofied <gustofied@gmail.com>
2025-12-25 00:03:50 +0100

    b

--- monde/numpy/one_basics.py
+++ monde/numpy/one_basics.py
@@ -1,52 +1,7 @@
 import numpy as np
 
-# vector = np.array([1, 2, 3, 4, 5, 6])
-# matrix = np.array([[1, 2, 3],[3, 5, 6]])
+arrayen2 = np.arange(,8, 1)
 
-# print(vector)
-# print(matrix)
 
-# arr = np.array([1, 2, 3, 4, 5, 6])
-# arr[2] = 22
-# b = np.append(arr, 2)
-# print(b)
-# print(b[:2:3])
-
-# np_list= np.array([1, 2, 3, 5, 6])
-# np_slice = np_list[:3]
-
-# np_slice[2] = 999
-# print(np_list)
-
-# matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# print(matrix[1][2])
-
-# arr = np.array([[1, 2, 3], [4, 5, 6], [4, 5, 6], [4, 5, 6]])
-# print(arr)
-
-# print('Diensions:', arr.ndim)
-# print('Shape', arr.shape)
-# print('Size', arr.size)
-# print('Type', arr.dtype)
-
-# arr = np.array([1, 4, 6, 8, 3, 2])
-# print(arr.sort())
-# print(np.sort(arr))
- 
-# a = np.array([1, 2, 3])
-# b = np.array([2, 4, 6])
-
-# ab = np.concatenate(a,b)
-# print(ab)
-
-# z = np.zeros(16)
-# print(z)
-# z = z.reshape(2,2,2,2)
-
-# print(z.ndim, z.dtype, z.size, z.shape, sep=" * ")
-
-# x = np.array([[1, 4, 2, 7], [1, 4, 2, 7]])
-# y = np.array([[8, 3, 10, 2], [ 4, 2, 7, 2]])
-# print(x.shape)
-# xy = np.concatenate([x,y], axis=1)
-# print(xy)
+print
+print(arrayen2)
