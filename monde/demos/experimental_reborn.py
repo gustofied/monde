043e2ea540dbb5b96d3c1f8a6ac8d77b6a93e70f
gustofied <gustofied@gmail.com>
2026-01-15 22:36:37 +0100

    reborn

---
+++ monde/demos/experimental_reborn.py
@@ -0,0 +1,46 @@
+import numpy as np
+import OpenGL.GL as GL
+from OpenGL.GL.shaders import compileShader
+import ctypes
+import glfw
+import glfw.GLFW as GLFW_CONSTANTS
+from pathlib import Path
+
+# Let's begin with window
+
+glfw.init()
+glfw.window_hint(GLFW_CONSTANTS.GLFW_CONTEXT_VERSION_MAJOR, 3)
+glfw.window_hint(GLFW_CONSTANTS.GLFW_CONTEXT_VERSION_MINOR, 3)
+glfw.window_hint(GLFW_CONSTANTS.GLFW_OPENGL_FORWARD_COMPAT, GLFW_CONSTANTS.GLFW_TRUE)
+window = glfw.create_window(400, 400, "reborn", None, None)
+glfw.make_context_current(window)
+glfw.swap_interval(0)
+
+
+GL.glClearColor(0.32, 0.31, 0.30, 1)
+
+
+old_time = glfw.get_time()
+
+while not glfw.window_should_close(window):
+
+    glfw.poll_events()
+
+    dif_timer = glfw.get_time() - old_time
+    fps = 1 / dif_timer
+    old_time = glfw.get_time()
+
+    if glfw.get_key(window, GLFW_CONSTANTS.GLFW_KEY_ESCAPE) == GLFW_CONSTANTS.GLFW_PRESS:
+        glfw.set_window_should_close(window, True)
+        
+    GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+    glfw.set_window_title(window, F"FPS is {fps:.2f}")
+
+    glfw.swap_buffers(window)
+
+
+glfw.terminate()
+
+
+
+
--- monde/numpy/one_basics.py
+++ monde/numpy/one_basics.py
@@ -1,7 +1,25 @@
 import numpy as np
 import timeit as timeit
 
-print(timeit.timeit("np.sort([1, 4, 5, 8, 10, 4])", globals=globals(), number=10))
 
+rng = np.random.default_rng(seed=13)
+
+
+
+arrayen = rng.choice(
+    500,
+    (2, 2, 4),
+    replace=False
+)
+
+
+
+print("- - - - - - ")
+
+
+print(arrayen)
+
+print(arrayen[1, 1, 1])
+print(arrayen[[[0, 0, 0]]])
 
 
