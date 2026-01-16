dc442feb6e7dfec51c1f254aef8fad107a5d5c45
gustofied <gustofied@gmail.com>
2026-01-16 20:05:42 +0100

    asd

--- monde/demos/experimental_reborn.py
+++ monde/demos/experimental_reborn.py
@@ -19,7 +19,79 @@ glfw.swap_interval(0)
 # oki our window is done, now we have to do what's next and that's perhaps create some shaders, and compile them
 
 
+
+my_dir = Path(__file__).resolve().parent
+print(my_dir)
+
+pathen_to_vertex = my_dir / "shaders" / "experimental_reborn_vertex.txt"
+pathen_to_fragment = my_dir / "shaders" / "experimental_reborn_fragment.txt"
+
+# now lets open and read them
+
+with open(pathen_to_vertex, "r") as file:
+    vertex_shader = file.read()
+
+print(vertex_shader)
+
+with open(pathen_to_fragment, "r") as file:
+    fragment_vertex = file.read()
+
+print(fragment_vertex)
+
+# now let's compile them
+
+myvertext = GL.glCreateShader(GL.GL_VERTEX_SHADER)
+GL.glShaderSource(myvertext, vertex_shader)
+GL.glCompileShader(myvertext)
+
+
+myfragment = GL.glCreateShader(GL.GL_FRAGMENT_SHADER)
+GL.glShaderSource(myfragment, fragment_vertex)
+GL.glCompileShader(myfragment)
+
+
+
+programme = GL.glCreateProgram()
+GL.glAttachShader(programme, myvertext)
+GL.glAttachShader(programme, myfragment)
+GL.glLinkProgram(programme)
+
+
+GL.glDeleteShader(myvertext)
+GL.glDeleteShader(myfragment)
+
+
 GL.glClearColor(0.32, 0.31, 0.30, 1)
+
+
+# some data
+
+arrayen = np.array([
+    [ 0.75,  0.75, 0.0],
+    [ 0.75, -0.75, 0.0],
+    [-0.75, -0.75, 0.0]
+], dtype=np.float32)
+
+vao = GL.glGenVertexArrays(1)
+vbo = GL.glGenBuffers(1)
+
+GL.glBindVertexArray(vao)
+
+GL.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo)
+GL.glBufferData(GL.GL_ARRAY_BUFFER, arrayen.nbytes, arrayen, GL.GL_STATIC_DRAW)
+
+GL.glVertexAttribPointer(
+    0,                   
+    3,                    
+    GL.GL_FLOAT,    
+    GL.GL_FALSE,          
+    3 * 4,           
+    ctypes.c_void_p(0)      
+)
+GL.glEnableVertexAttribArray(0)
+
+
+GL.glUseProgram(programme)
 
 
 old_time = glfw.get_time()
@@ -37,6 +109,10 @@ while not glfw.window_should_close(window):
         
     GL.glClear(GL.GL_COLOR_BUFFER_BIT)
     glfw.set_window_title(window, F"FPS is {fps:.2f}")
+
+    GL.glBindVertexArray(vao)
+    GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
+    GL.glBindVertexArray(0)
 
     glfw.swap_buffers(window)
 
